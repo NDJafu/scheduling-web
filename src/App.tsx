8fbcd8385f9a3b160931154b14a537f8a4408a5d
NDJafu <giaphu432@gmail.com>
2024-10-02 17:45:38 +0700

    kys

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import NotesPage from "./pages/Notes.page";
+import { ThemeProvider } from "./components/theme-provider";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <NotesPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="bg-green-500">kys</div>;
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="app-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
